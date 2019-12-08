2eda076de3c42b3f5ce0fd4ba2ec36f3068de8f8
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-12-08 02:15:28 +0100

    :bug: Add memory for function

--- functions/index.js
+++ functions/index.js
@@ -17,22 +17,32 @@ const selectHtmlPage = async () => {
   }
 }
 
-const ssr = functions.https.onRequest(async (request, response) => {
-  if (request.path.endsWith('/favicon.ico')) {
-    response.status(404).end()
-  } else {
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
-    const html = await selectHtmlPage()
-    await page.goto('http://localhost:5000/index.html')
-    const { path } = request
-    await page.evaluate(path => window.history.pushState({}, '', path), path)
-    await page.setContent(html, {
-      waitUntil: ['networkidle0', 'domcontentloaded'],
-    })
-    const content = await page.content()
-    response.send(content)
-    await page.close()
+const runtimeOptions = {
+  memory: '2GB',
+  timeoutSeconds: 300,
+}
+
+const ssr = functions.runWith(runtimeOptions).https.onRequest(async (request, response) => {
+  try {
+    if (request.path.endsWith('/favicon.ico')) {
+      response.status(404).end()
+    } else {
+      const browser = await puppeteer.launch({ headless: true })
+      const page = await browser.newPage()
+      const html = await selectHtmlPage()
+      await page.goto('http://localhost:5000/index.html')
+      const { path } = request
+      await page.evaluate(path => window.history.pushState({}, '', path), path)
+      await page.setContent(html, {
+        waitUntil: ['networkidle0', 'domcontentloaded'],
+      })
+      const content = await page.content()
+      response.send(content)
+      await page.close()
+    }
+  } catch (error) {
+    console.warn(error)
+    response.status(500).end()
   }
 })
 
